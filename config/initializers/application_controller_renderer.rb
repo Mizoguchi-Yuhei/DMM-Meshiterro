0fee834d64864031c6b48fa0b5e46228e895dc2e
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-05-02 05:34:50 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '043fc29e27989982e215c04087cfb1dfd182821c6719049af98a68fa6124bcfbd996bcfe003b0881b187aaf69c0e900e24f94eb77066db9f4b15f37d19a342a8'
